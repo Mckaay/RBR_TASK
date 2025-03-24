67fc12d77e72fa89a38a1b10bdea448e6d824ed1
Michal <michal1nycz@gmail.com>
2025-03-24 12:58:53 +0100

    Add Skeleton

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+final class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void {}
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        $this->configureCommands();
+        $this->configureModels();
+    }
+
+
+    private function configureCommands(): void
+    {
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction(),
+        );
+    }
+
+    private function configureModels(): void
+    {
+        Model::shouldBeStrict();
+        Model::preventLazyLoading();
+        Model::unguard();
+    }
+}
